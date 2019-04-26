4ba0cda8cf2226df42b936faee7d67acf04f1844
Kasam Dhakal <kasamdh@hotmail.com>
2019-04-25 23:06:44 -0400

    prints the first three characters of every word

---
+++ threeChar.py
@@ -0,0 +1,18 @@
+'''
+Write a for loop that iterates over a list of strings lst and prints the first three characters of every word.
+If lst is the list ['January', 'February', 'March'] then the following should be printed:
+'''
+
+lst=['January', 'February', 'March']
+
+for i in lst:
+    print(i[:3])
+
+
+'''
+Output:
+Jan
+Feb
+Mar
+
+'''
