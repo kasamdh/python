c75f9ad74fc483368709b935995e0cfbf602369d
Kasam Dhakal <kasamdh@hotmail.com>
2019-04-26 23:01:02 -0400

    swap first and last element of the list

---
+++ swap.py
@@ -0,0 +1,19 @@
+'''
+Implement function swapFL() that takes a list as input and swaps the first and last ele- ments of the list.
+You may assume the list will be nonempty.
+The function should not return anything.
+>>> ingredients = ['flour', 'sugar', 'butter', 'apples']
+>>> swapFL(ingredients)
+>>> ingredients
+['apples', 'sugar', 'butter', 'flour']
+'''
+def swapFL(ingredients):
+    ingredientFirst=ingredients[0]
+    ingredientLast=ingredients[-1]
+
+    ingredients[0]= ingredientLast
+    ingredients[-1]=ingredientFirst
+
+    return(ingredients)
+ingredients = ['flour', 'sugar', 'butter', 'apples']
+print(swapFL(ingredients))
